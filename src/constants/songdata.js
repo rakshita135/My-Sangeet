c7672d1faa26ffe426e9d7646c097bd4c177fe72
rakshita135 <rakshitamehta2005@gmail.com>
2023-08-27 15:23:19 +0530

    added dynamic cards and buttons

---
+++ src/constants/songdata.js
@@ -0,0 +1,27 @@
+const SongData=[
+    {
+        id:1,
+        coverimg:'concert1.jpg',
+        songname:'Zindagi Sawar Du',
+        singer:'Doraemon'
+    },
+    {
+        id:2,
+        coverimg:'concert2.jpeg',
+        songname:'Jab HUM bade ho',
+        singer:'Nobita'
+    },
+    {
+        id:1,
+        coverimg:'concert3.jpeg',
+        songname:'Mai hu Jiyaan',
+        singer:'Jiyaan'
+    },
+    {
+        id:1,
+        coverimg:'cover1.jpg',
+        songname:'Zindagi Sawar Du',
+        singer:'Doraemon'
+    },
+]
+export default SongData;
--- src/my-comp/HomePage.js
+++ src/my-comp/HomePage.js
@@ -1,13 +1,38 @@
+import SongData from '../constants/songdata';
 import './HomePage.css';
+import MediaCard from './card';
+
 function HomePage() {
     return (
         <>
-          <div class="scroll-container">
-  <img src="\concert1.jpg" alt="Cinque Terre" width="600" height="400"/>
-  <img src="\concert2.jpeg" alt="Forest" width="600" height="400"/>
-  <img src="\concert3.jpeg" alt="Northern Lights" width="600" height="400"/>
- 
+<div class="scroll-container">
+  <img src="\concert1.jpg" alt="Cinque Terre" className='image' width="600" height="400"/>
+  <img src="\concert2.jpeg" alt="Forest" className='image' width="600" height="400"/>
+  <img src="\concert3.jpeg" alt="Northern Lights" className='image' width="600" height="400"/>
+  <img src="\logo1.webp" alt="Northern Lights" className='image' width="600" height="400"/>
 </div>
+  
+        <div className='backimage'>
+<div className='text2'>Music is the Medicine of Mind.</div>
+<div id='buttons'>
+<button type="button" class="btn btn-outline-warning">Melodies</button>
+<button type="button" class="btn btn-outline-warning">90's HIts</button>
+<button type="button" class="btn btn-outline-warning">Energetic</button>
+<button type="button" class="btn btn-outline-warning">Bhakti</button>
+</div>
+</div>
+<div id='diy-cards' className='col-6'>
+        {SongData.map((song,index)=>{
+                return <><MediaCard song={song.songname}
+                                    singer={song.singer}
+                                    image={song.coverimg}/>
+                </>
+        })}
+</div>
+
+
+
+
 
         </>
     );
